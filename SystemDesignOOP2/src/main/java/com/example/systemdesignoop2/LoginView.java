fd88db545052d51d5f490f88306595034cd60475
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2024-11-20 10:57:13 -0500

    code works, will need to prettify

--- SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/LoginView.java
+++ SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/LoginView.java
@@ -1,9 +1,11 @@
 package com.example.systemdesignoop2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class LoginView {
+
     @FXML
     private TextField usernameTextField;
     @FXML
@@ -17,11 +19,64 @@ public class LoginView {
     @FXML
     private RadioButton managerRadioButton;
     @FXML
-    private ToggleGroup userToggleGroup;
-    @FXML
     private Label usernameLabel;
     @FXML
     private Label passwordLabel;
     @FXML
     private Label signUpLabel;
+
+    @FXML
+    public void initialize() {
+        // Initialize ToggleGroup for radio buttons
+        // Declare ToggleGroup
+        ToggleGroup userToggleGroup = new ToggleGroup();
+        clientRadioButton.setToggleGroup(userToggleGroup);
+        managerRadioButton.setToggleGroup(userToggleGroup);
+    }
+
+    @FXML
+    public void onClientRadioButtonClick(ActionEvent event) {
+        // No need to manually select; ToggleGroup will handle it
+    }
+
+    @FXML
+    public void onManagerRadioButtonClick(ActionEvent event) {
+        // No need to manually select; ToggleGroup will handle it
+    }
+
+    @FXML
+    protected void onUsernameTextFieldClick(ActionEvent event) {
+        usernameTextField.setText("");
+    }
+
+    @FXML
+    protected void onPasswordTextFieldClick(ActionEvent event) {
+        passwordTextField.setText("");
+    }
+
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Sign Up");
+        alert.setHeaderText("Do you want to create a new account?");
+        alert.showAndWait();
+    }
+
+    @FXML
+    protected void onLoginButtonClick(ActionEvent event) {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Username and Password are required!");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Login");
+            alert.setHeaderText("Welcome, " + username + "!");
+            alert.showAndWait();
+        }
+    }
 }
