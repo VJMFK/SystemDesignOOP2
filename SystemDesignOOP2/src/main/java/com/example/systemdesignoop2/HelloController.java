8b2317538585f789c62f920f5a9ff3d1a37a0906
Joey Knowles <Joey.Knowles@stu.crc-lennox.qc.ca>
2024-11-20 22:41:14 -0500

    Created view to access all the manager sections.

--- SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/HelloController.java
+++ SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.systemdesignoop2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button movieListButton;
+
+    @FXML
+    private Button screeningListButton;
+
+    @FXML
+    private Button showtimesListButton;
+
+    @FXML
+    private Button ticketSalesButton;
+
+    @FXML
+    private Button clientListButton;
+
+
+    protected void onMovieListButtonClick(ActionEvent actionEvent) {
+
+    }
+
+    protected void onScreeningListButtonClick(ActionEvent actionEvent) {
+
+    }
+
+    protected void onShowtimesListButtonClick(ActionEvent actionEvent) {
+
+    }
+
+    protected void onTicketSalesButtonClick(ActionEvent actionEvent) {
+
+    }
+
+    protected void onClientListButtonClick(ActionEvent actionEvent) {
+
     }
 }
