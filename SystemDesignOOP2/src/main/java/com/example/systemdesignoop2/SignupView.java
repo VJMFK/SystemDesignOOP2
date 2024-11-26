1567f4da47e16f39ce5d839974861bd62437d679
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2024-11-25 19:14:31 -0500

    something for the textfields

--- SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/SignupView.java
+++ SystemDesignOOP2/src/main/java/com/example/systemdesignoop2/SignupView.java
@@ -1,6 +1,8 @@
 package com.example.systemdesignoop2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -18,5 +20,16 @@ public class SignupView {
     private Label newPassword;
 
     @FXML
+    protected void onNewUsernameTextFieldClick(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("New Username");
+        alert.showAndWait();
+    }
+    @FXML
+    protected void onNewPasswordTextFieldClick(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("New Password");
+        alert.showAndWait();
+    }
 
 }
